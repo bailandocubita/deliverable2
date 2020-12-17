05327b5be8ec8dea9fef5806b1d86c37c34a5d99
kate <katemorawoods@gmail.com>
2020-12-16 23:20:42 -0500

    First commit for Deliverable 2

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//console.log("hello!");
+
+let rainFall = 0;
+let fertilizer = [];
+let fertilizerType = [];
+
+
+
+rainFall = prompt("How many inches of rain fell?", "Please enter a number.");
+rainFall = "*".repeat(rainFall)
+console.log(rainFall)
+
+prompt("Did you use fertilizer?", "Yes or No");
+    if("Yes" || "yes") {
+        prompt("Did you use premium or regular fertilizer?");
+    }
+    
+
+
+//var fertilizer = prompt("Did you use fertilizer?", "Yes or No");
+//if (fertilizer === "yes") {}
+
+
+//var fertilizerType = prompt("Did you use premium or regular fertilizer?");
